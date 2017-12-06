08ba306c78ecf078284f26d97d0cdea8276c8bdf
Prashant <ppresh.ant@gmail.com>
2017-12-06 09:06:17 -0600

    Added all 3 functions, can iterate over 2 steps easily and plot all in the same graph

--- ode_simulations.py
+++ ode_simulations.py
@@ -10,17 +10,19 @@ import numpy as np
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 
-# Rate constants
-du = 1; k2 = 1; k3 = 1;
-g1 = 2; g2 = 3; g3 = 1;
+# Rate constants : System parameters
+du = 1.0; k2 = 1.0; k3 = 1.0;
+g1 = 2.0; g2 = 3.0; g3 = 1.0;
 
 # control parameters
-k = .1; n = 1; mu = 3; theta = 1; # general
+k = .1; n = 1.0; mu = 3.0; theta = 10.0; # general
 k4 = 0; k5 = 0; g4 = 0; g5 = 0; # synth and degradation of control species
-km = 1; h = 1;  # hill fn
+km = 1.0; h = 1.0;  # hill fn
 
-tmul = 100 ; # time to equilibriate the disturbances
+tmul = 200 ; # time to equilibriate the disturbances
 par = 'k2' # parameter of interest
+parscale = 40.0 # how much should the parameter be increased
+
 dp = {'k': k,'k2': k2,'k3': k3,'g1': g1,'g2': g2,'g3': g3,'du' : du, 'mu' : mu,'theta':theta,'n' : n, 'km' : km,'h' : h, 'k4' : k4, 'k5' : k5, 'g4' : g4, 'g5' : g5} # dictionary with all parameters
 
 # differential equation sets
@@ -38,6 +40,31 @@ def anti(X, t, *param):
     c = np.array([du, 0, 0, theta*mu, 0])                 
     return np.dot(Ab, X) + c
 
+def hill(X, t, *param):
+    
+    [mu, g5, g4, g3, g2, g1, h, k, km, n, k3, k2, k5, k4, theta, du] = param;
+    theta = 2.5;
+    Ab = np.array([[-g1,    0,    0,    0,  0],  
+               [ k2, -(g2 + k3),  0,    0,  0],
+               [ 0,     k3,      -g3,   0,  0],
+               [ 0,     0,   -theta*X[3],  -k4, 0],
+               [0,  0,  0,  0,  0]])         
+             
+    c = np.array([du + k*pow(X[3]/km,h)/(1+pow(X[3]/km,h)) , 0, 0 , theta*mu,   0])      # constant production terms            
+    return np.dot(Ab, X) + c
+
+def intgl(X, t, *param):
+    
+    [mu, g5, g4, g3, g2, g1, h, k, km, n, k3, k2, k5, k4, theta, du] = param;
+    theta = .04
+    Ab = np.array([[-g1,    0,        0,    k,  0],  
+               [ k2, -(g2 + k3),  0,    0,  0],
+               [ 0,     k3,      -g3,   0,  0],
+               [ 0,     0,   -theta*X[3],  theta*mu,    0],
+               [0,0,0,0,0]])
+    c = np.array([du,    0,        0,    0, 0])                 
+    return np.dot(Ab, X) + c
+    
 # Solving the ODEs for the reaction given the rate consts
 # and initial conditions
 def OdeReact(deriv,t0,t1,X0, dpar,par):
@@ -104,5 +131,70 @@ def quadsteps():
     return 0
 
 
+def twosteps(contrl):
+    # first step
+    
+    X0 = np.array([0.1, 1, .1, .1, .1])
+    time1, MA1, sp1, p1 = OdeReact(contrl,0,tmul,X0,dp,par)
+    
+    
+    # second step
+    
+    dp[par] = dp[par]* parscale
+    time2, MA2, sp2, p2 = OdeReact(contrl,tmul,3*tmul,MA1[-1],dp,par)
+    
+    # Pack all steps into 1 vector / matrix 
+    
+    MAf = np.row_stack([MA1,MA2])
+    tf = np.append(time1,time2)
+    spf = sp1+sp2
+    pf = p1 + p2 
+    
+#    plt.plot(tf,MAf[:,-3:-2]) # plot x3
+#    plt.ylabel('Y(t)')
+#    plt.xlabel('Time (s)')
+    
+#    plt.plot(tf,spf,'--k', linewidth=1)
+#    plt.plot(tf,pf,':r', linewidth=2)
+#    plt.legend(['x3','Mu',par],loc='best')
+#    plt.title('System perturbation : Antithetical network')
+    #plt.title('Near Perfect Adaptation')
+    
+    plt.plot(tf,MAf[:,-3:]) # plot v and x3
+    plt.ylabel('Y(t)')
+    plt.xlabel('Time (s)')
+    plt.plot(tf,spf,'--k', linewidth=1)
+    plt.plot(tf,pf,':r', linewidth=2)
+    plt.legend(['x3','Z1','Z2','Mu',par],loc='upper right')
+    plt.title('Debugging')
+    
+    
+    dp[par] = dp[par]/ parscale # return the parameter to original value
+    return [tf,spf,pf]
+
+twosteps(anti)
+## Hill step
+#    
+#twosteps(hill)
 
 
+# Anti step
+
+#twosteps(anti)
+#
+## intgl step
+#
+#[tf, spf,pf] = twosteps(intgl)
+#
+## Pack all steps into 1 vector / matrix 
+#
+##MAf = np.row_stack([MA1,MA2])
+##tf = np.append(time1,time2)
+##spf = sp1+sp2
+##pf = p1 + p2 
+#
+#
+#plt.plot(tf,spf,'--k', linewidth=1)
+#plt.plot(tf,pf,':r', linewidth=2)
+#plt.legend(['Antithetical','Integral','Mu',par],loc='best')
+#plt.title('Comparision of controller architectures')
